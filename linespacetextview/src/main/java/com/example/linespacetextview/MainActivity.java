872582289008c12742d61bb04b7154241140cb4f
王英豪 <wyh3@meitu.com>
2018-08-11 14:09:12 +0800

    段落间距

--- linespacetextview/src/main/java/com/example/linespacetextview/MainActivity.java
+++ linespacetextview/src/main/java/com/example/linespacetextview/MainActivity.java
@@ -7,8 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ImageSpan;
-import android.util.Log;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,70 +21,102 @@ public class MainActivity extends AppCompatActivity {
     String TAG = "MainActivity";
     private TextView mTv;
 
-    int lineSpacingExtra = 5;
-    int paragraphSpacing = 3;
+    private EditText mEt1;
+    private EditText mEt2;
+    private EditText mEt3;
 
-    String text = "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n" +
-            "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n" +
-            "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n" +
+
+    /**
+     * 行间距offset
+     */
+    int lineSpacingExtraDp = 0;
+    /**
+     * 段落间距offset
+     */
+    int paragraphSpacingDp = 3;
+
+
+    String text = "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n" +
+            "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n" +
+            "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n" +
             "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTv = (TextView) findViewById(R.id.tv);
+        mTv = findViewById(R.id.tv);
+
+        mEt1 = findViewById(R.id.et1);
+        mEt2 = findViewById(R.id.et2);
+        mEt3 = findViewById(R.id.et3);
 
 
-        mTv.setText(insertSpaceSpan(mTv, text));
+        mEt1.setText("15");
+        mEt2.setText("0");
+        mEt3.setText("3");
+
+//        mTv.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                change();
+//            }
+//        });
+
+        change();
+
+
     }
 
 
-    private CharSequence insertSpaceSpan(TextView textView, String content) {
+    void change() {
+        mTv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, getDp(mEt1));
+        mTv.setLineSpacing(dip2px(getDp(mEt2)), 1);
+        mTv.setText(insertSpaceSpan(mTv, text, dip2px(getDp(mEt3))));
+    }
+
+
+    public void click(View view) {
+        change();
+    }
+
+
+    private CharSequence insertSpaceSpan(TextView textView, String content, int paragraphSpacingOffset) {
         if (!content.contains("\n")) {
             return content;
         }
         content = content.replace("\n", "\n\r");
-
         int previousIndex = content.indexOf("\n\r");
-        //记录每个段落开始的index，第一段没有，从第二段开始
         List<Integer> nextParagraphBeginIndexes = new ArrayList<>();
         nextParagraphBeginIndexes.add(previousIndex);
         while (previousIndex != -1) {
-            int nextIndex = content.indexOf("\n\r", previousIndex + 2);
-            previousIndex = nextIndex;
+            previousIndex = content.indexOf("\n\r", previousIndex + 2);
             if (previousIndex != -1) {
                 nextParagraphBeginIndexes.add(previousIndex);
             }
         }
         //获取行高（包含文字高度和行距）
         float lineHeight = textView.getLineHeight();
-
         //把\r替换成透明长方形（宽:1px，高：字高+段距）
         SpannableString spanString = new SpannableString(content);
         Drawable d = ContextCompat.getDrawable(this, R.drawable.paragraph_space);
-        float density = getResources().getDisplayMetrics().density;
-        //int强转部分为：行高 - 行距 + 段距
-        int bottom = (int) ((lineHeight - lineSpacingExtra * density) / 1.2 + (paragraphSpacing - lineSpacingExtra) * density);
-        d.setBounds(0, 0,
-                1, 70);
+        d.setBounds(0, 0, 0, (int) (lineHeight + paragraphSpacingOffset));
         for (int index : nextParagraphBeginIndexes) {
-            // \r在String中占一个index
-            Log.d(TAG, "insertSpaceSpan: index=" + index);
-            spanString.setSpan(new ImageSpan(d), index + 2, index + 3, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+            spanString.setSpan(new ImageSpan(d), index + 1, index + 2, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         }
         return spanString;
     }
 
 
-    public void click(View view) {
-        mTv.setText(insertSpaceSpan(mTv, text));
-//        SpannableString spanString = new SpannableString(text);
-//        Drawable d = ContextCompat.getDrawable(this, R.drawable.paragraph_space);
-//        d.setBounds(0, 0,
-//                5, 100);
-//        spanString.setSpan(new ImageSpan(d), 2, 3, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-//        mTv.setText(spanString);
-
+    public int dip2px(float dipValue) {
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        return (int) (dipValue * dm.density + 0.5f);
     }
+
+
+    private int getDp(EditText editText) {
+        String t = editText.getText().toString();
+        return Integer.parseInt(t);
+    }
+
 }
